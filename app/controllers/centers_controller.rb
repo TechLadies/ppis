96c9ed058537e6616f4a89499fa35a75da974611
NatAirGit <cheryesque@gmail.com>
2016-09-27 14:58:30 +0800

    change settings of editor to use 2 spaces instead of 4, modify seed file to utilise text wrap to 120 characters and remove unit test files.

--- app/controllers/centers_controller.rb
+++ app/controllers/centers_controller.rb
@@ -6,3 +6,13 @@ def show
   @center = Center.find(params[:id])
 end
 end
+	def create
+		@center = Center.find(params[:center_id])
+		redirect_to center_path(@center)
+	end
+
+	private
+	def center_params
+		params.require(:center).permit(:moderator, :address)
+	end
+end

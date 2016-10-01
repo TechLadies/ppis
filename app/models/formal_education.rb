2ec1ff1ecb5c5267dfdc5d8a5701e265d8297fc5
Sherwin Chua <anthonysherwinchua@gmail.com>
2016-10-01 13:44:07 +0800

    apply rubocop autocorrect

--- app/models/formal_education.rb
+++ app/models/formal_education.rb
@@ -1,3 +1,3 @@
 class FormalEducation < ApplicationRecord
-	validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end

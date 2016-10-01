3a2b44f5956d3dcf922eeaed5b227c649bc542e8
Sherwin Chua <anthonysherwinchua@gmail.com>
2016-10-01 14:24:18 +0800

    remove unnecessary files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
   devise_for :volunteers
-  root to: "page#index"
+  root to: "pages#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/page_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PageControllerTest < ActionDispatch::IntegrationTest
-  test 'should get test' do
-    get page_test_url
-    assert_response :success
-  end
-
-end

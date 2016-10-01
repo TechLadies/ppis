55c5e5e28595f0522ad44d167a1acf48ccdd38c5
lingadingdong <lphang1@e.ntu.edu.sg>
2016-10-01 17:27:06 +0800

    Correct t.references

--- db/migrate/20160927160047_create_preferred_availabilities.rb
+++ db/migrate/20160927160047_create_preferred_availabilities.rb
@@ -1,8 +1,8 @@
 class CreatePreferredAvailabilities < ActiveRecord::Migration[5.0]
   def change
     create_table :preferred_availabilities do |t|
-      t.references :availability_id
-      t.references :volunteer_id
+      t.references :availability
+      t.references :volunteer
 
       t.timestamps
     end

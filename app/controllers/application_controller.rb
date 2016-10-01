be931858274960bb09c97558c4d881e87222caf4
Sherwin Chua <anthonysherwinchua@gmail.com>
2016-10-01 13:55:26 +0800

    apply rubocop autocorrect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
-  
-    protect_from_forgery with: :exception
 
-    before_action :authenticate_volunteer!
-    
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
 
-protected
+  before_action :authenticate_volunteer!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit([:name,:gender,:date_of_birth,:mobile,:profession,
-      :other_talents,:about_me])
+    devise_parameter_sanitizer.permit([:name, :gender, :date_of_birth, :mobile, :profession,
+                                       :other_talents, :about_me])
   end
-  
-end
 
+end
--- test/controllers/page_controller_test.rb
+++ test/controllers/page_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class PageControllerTest < ActionDispatch::IntegrationTest
-  test "should get test" do
+  test 'should get test' do
     get page_test_url
     assert_response :success
   end

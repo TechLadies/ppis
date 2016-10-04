602f535dbf8d0c02bacece80fde9a41bd4febd68
Nat <cheryesque@gmail.com>
2016-10-04 12:10:39 +0800

    reorganise seed file of formal-educations branch

--- db/seeds/formal_educations.rb
+++ db/seeds/formal_educations.rb
@@ -1,16 +1,6 @@
-formal_educations = [
-  "N Level",
-  "O Level",
-  "A Level",
-  "NITEC",
-  "Higher NITEC",
-  "Diploma",
-  "Degree",
-  "Master",
-  "PhD",
+formal_educations = ["N Level", "O Level", "A Level", "NITEC", "Higher NITEC", "Diploma", "Degree", "Master", "PhD",
   "Other"
 ]
-
 formal_educations.each do |education|
   FormalEducation.create(name: education)
 end

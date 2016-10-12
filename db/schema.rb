010d259f7d61da2b9195b9c594a44a7202914a7a
G115H <hillsgemma@gmail.com>
2016-10-12 11:34:47 +0800

    add db/schema.rb update

--- db/schema.rb
+++ db/schema.rb
@@ -103,6 +103,11 @@ ActiveRecord::Schema.define(version: 20161005033954) do
     t.string   "profession"
     t.text     "other_talents"
     t.text     "about_me"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_volunteers_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_volunteers_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_volunteers_on_reset_password_token", unique: true, using: :btree
   end

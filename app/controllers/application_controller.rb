6aa3f3c0aaec7538482c1d43eb62063f7c447bde
Nat <cheryesque@gmail.com>
2016-10-16 16:27:02 +0800

    remove unnecessary files as follow 1. admin_spec.rb 2. admins.rb 3. app/models/concerns/Accessible 4. config/application.rb because mail for dev has been set up in config/environments/development.rb also remove commented lines from application controller and add admin.coffee into app assets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  # before_action :authenticate_volunteer!
-
   def after_sign_in_path_for(resource)
     if current_admin
       admin_root_path
--- config/application.rb
+++ config/application.rb
@@ -11,15 +11,6 @@ module PPIS
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-  ActionMailer::Base.smtp_settings = {
-      :address              => "smtp.gmail.com",
-      :domain               => "mail.google.com",
-      :port                 => 587,
-      :user_name            => "xyz@gmail.com",
-      :password             => "yourpassword",
-      :authentication       => 'login',
-      :enable_starttls_auto => true
-}    
+ 
   end
 end

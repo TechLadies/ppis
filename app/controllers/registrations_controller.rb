ac4b62c5f02cd6ee3691896225dc420d405db367
lingadingdong <lphang1@e.ntu.edu.sg>
2016-10-17 23:29:07 +0800

    Enhancement/availabilities (#27)
    * Add availability to sign up page
    
    * add schema
    
    * add availability_ids in strong parameters in registration controller
    
    * Revert "add schema"
    
    This reverts commit 870adf7071e275ca50be147a6690fb981d723fab.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,6 @@ class RegistrationsController < Devise::RegistrationsController
   def sign_up_params
     params.require(:volunteer).permit(:name, :gender, :"date_of_birth(3i)", :"date_of_birth(2i)", :"date_of_birth(1i)",
       :mobile, :profession, :other_talents, :about_me, :email, :password, :password_confirmation,
-      target_group_ids: [], skill_ids: [], center_ids: [], formal_education_ids: [])
-  end
+      target_group_ids: [], availability_ids: [], skill_ids: [], center_ids: [], formal_education_ids: [])
 
 end

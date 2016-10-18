b5d00aada349fff30cda1c63399e3240c7de357b
lingadingdong <lphang1@e.ntu.edu.sg>
2016-10-18 19:31:10 +0800

    generate model for certification

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161016160253) do
+ActiveRecord::Schema.define(version: 20161018111931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,12 @@ ActiveRecord::Schema.define(version: 20161016160253) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.string   "fax_number"
+  end
+
+  create_table "certifications", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "formal_educations", force: :cascade do |t|

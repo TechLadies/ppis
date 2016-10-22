4d0d3ae0c70a43afb3af80076ef30fe80036f88d
Sherwin Chua <anthonysherwinchua@gmail.com>
2016-10-22 15:12:15 +0800

    fix sign in/up redirections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,16 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def after_sign_in_path_for(_resource)
-    if current_admin
+  def after_sign_in_path_for(resource)
+    if resource.is_a? Admin
       admin_root_path
+    else
+      root_path
     end
+  end
+
+  def after_sign_up_path_for(_resource)
+    root_path
   end
 
 end

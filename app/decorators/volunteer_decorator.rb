cd9e2343978087a10656df95214b9281261c2c5b
G115H <hillsgemma@gmail.com>
2016-10-31 22:29:27 +0800

    add fields to show view and decorator

--- app/decorators/volunteer_decorator.rb
+++ app/decorators/volunteer_decorator.rb
@@ -1,5 +1,29 @@
 class VolunteerDecorator < SimpleDelegator
 
+  def date_of_birth
+    if super.blank?
+       'Not given'
+    else
+      super
+    end
+  end
+
+  def mobile
+    if super.blank?
+       'Not given'
+    else
+      super
+    end
+  end
+
+  def profession
+    if super.blank?
+       'Not given'
+    else
+      super
+    end
+  end
+
   def certifications
     if super.blank?
       'N/A'
@@ -24,6 +48,14 @@ class VolunteerDecorator < SimpleDelegator
     end
   end
 
+  def centers
+    if super.blank?
+      'N/A'
+    else
+      super.map(&:name).to_sentence
+    end
+  end
+
   def target_groups
     if super.blank?
       'N/A'
@@ -31,5 +63,30 @@ class VolunteerDecorator < SimpleDelegator
       super.map(&:name).to_sentence
     end
   end
+
+  def availabilities
+    if super.blank?
+      'N/A'
+    else
+      super.map(&:name).to_sentence
+    end
+  end
+
+  def other_talents
+    if super.blank?
+       'Not given'
+    else
+      super
+    end
+  end
+
+  def about_me
+    if super.blank?
+       'Not completed'
+    else
+      super
+    end
+  end
+
 
 end

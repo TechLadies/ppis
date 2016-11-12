7bad188522365bb713d97c91ac563f0f1d368c76
Nat <cheryesque@gmail.com>
2016-11-12 15:20:03 +0800

    add validation and auto calculate duration

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,4 +8,14 @@ class Event < ApplicationRecord
   validates :end_time, presence: true
   validates :location, presence: true
   validates :number_of_volunteers, presence: true
+  validates :duration, presence: true, numericality: { greater_than: 0 }
+
+  before_validation :calculate_duration
+
+  private
+
+  def calculate_duration
+    self.duration = end_time.to_i - start_time.to_i
+  end
+
 end

81e991fbc5d1fdb8078df63ff065e935cc0ffa2a
lingadingdong <lphang1@e.ntu.edu.sg>
2016-11-20 17:13:53 +0800

    add seed events to new events dashboard (#76)
    * add seed events to new events dashboard
    
    *  added show page for new events

--- app/controllers/my/events_controller.rb
+++ app/controllers/my/events_controller.rb
@@ -1,5 +1,22 @@
 class My::EventsController < My::BaseController
-  def upcoming_events
+  def new_events
     @events = Event.all
   end
+
+  def show
+    @event = find_event
+  end
+
+  private
+
+  def find_event
+    Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:center_id, :event_name, :description, :date, :start_time, :end_time, :location,
+      :number_of_volunteers)
+  end
+
 end
+

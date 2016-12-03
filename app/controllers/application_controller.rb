9948da168c0307ac56529b1558339d5f983e7069
lingadingdong <lphang1@e.ntu.edu.sg>
2016-12-03 15:40:57 +0800

    redirect guest user to register then back to current  event page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,10 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if resource.is_a? Admin
       admin_root_path
+    elsif params[:event_id]
+      event_path(params[:event_id])
     else
       my_root_path
     end
   end
-
-  def after_sign_up_path_for(_resource)
-    my_root_path
-  end
-
 end

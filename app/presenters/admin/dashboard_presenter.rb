c21c3f20866ed85cf5f13b9236c0942e12eeabb6
G115H <hillsgemma@gmail.com>
2017-01-08 14:54:19 +0800

    Correct data for events chart

--- app/presenters/admin/dashboard_presenter.rb
+++ app/presenters/admin/dashboard_presenter.rb
@@ -7,21 +7,21 @@ class Admin::DashboardPresenter
   def initialize
     @volunteers = all_volunteers.group(:adhoc).count
 
-    @volunteers_past3yrs_include_current = all_volunteers.where('created_at > ?', (Date.today - 2.years).beginning_of_year)
+    @volunteers_past3yrs_include_current = all_volunteers.where('created_at > ?', (Time.zone.today - 2.years).beginning_of_year)
 
     @volunteers_past3yrs_exclude_current = @volunteers_past3yrs_include_current.where('created_at < ?',
-     (Date.today - 1.year).end_of_year)
+     (Time.zone.today - 1.year).end_of_year)
 
     @volunteers_past3yrs_oldest_only = @volunteers_past3yrs_include_current.where('created_at < ?',
-      (Date.today - 2.years).end_of_year)
+     (Time.zone.today - 2.years).end_of_year)
 
-    @events_past3yrs_include_current = all_events.where('created_at > ?', (Date.today - 2.years).beginning_of_year)
+    @events_past3yrs_include_current = all_events.where('date > ?', (Time.zone.today - 2.years).beginning_of_year)
 
-    @events_past3yrs_exclude_current = @events_past3yrs_include_current.where('created_at < ?',
-     (Date.today - 1.year).end_of_year)
+    @events_past3yrs_exclude_current = @events_past3yrs_include_current.where('date < ?',
+     (Time.zone.today - 1.year).end_of_year)
 
-    @events_past3yrs_oldest_only = @events_past3yrs_include_current.where('created_at < ?',
-      (Date.today - 2.years).end_of_year)
+    @events_past3yrs_oldest_only = @events_past3yrs_include_current.where('date < ?',
+     (Time.zone.today - 2.years).end_of_year)
   end
 
   private

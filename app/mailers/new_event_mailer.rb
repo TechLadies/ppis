94f3fdede22ff09b355c1fcca143518928ff1905
G115H <hillsgemma@gmail.com>
2017-02-08 11:11:00 +0800

    complete html and text email templates and preview

--- app/mailers/new_event_mailer.rb
+++ app/mailers/new_event_mailer.rb
@@ -3,6 +3,6 @@ class NewEventMailer < ApplicationMailer
   def notify_volunteer(event, volunteer)
     @event = event
     @volunteer = volunteer
-    mail(to: @volunteer.email, subject: 'New event by PPIS')
+    mail(to: @volunteer.email, subject: "New event by PPIS #{@event.event_name}")
   end
 end

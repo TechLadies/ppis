cfd11a156612ade8dbfc346a6bab518a6177eb1f
G115H <hillsgemma@gmail.com>
2017-02-16 19:55:51 +0800

    make email notification and adhoc permitted params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,11 +3,13 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:volunteer).permit(:name, :gender, :"date_of_birth(3i)", :"date_of_birth(2i)", :"date_of_birth(1i)",
-      :mobile, :profession, :other_talents, :about_me, :email, :password, :password_confirmation, :formal_education_id,
-      :marital_status, :address, :home_phone_number, :language_spoken, :past_experience, :nric_no,
-      target_group_ids: [], availability_ids: [], skill_ids: [], center_ids: [], certification_ids: [],
-      email_notification: [true, false], adhoc: [true, false])
+    params.require(:volunteer).permit(
+    :name, :gender, :"date_of_birth(3i)", :"date_of_birth(2i)", :"date_of_birth(1i)",
+    :mobile, :profession, :other_talents, :about_me, :email, :password, :password_confirmation, :formal_education_id,
+    :marital_status, :address, :home_phone_number, :language_spoken, :past_experience, :nric_no, :adhoc,
+    :email_notification, target_group_ids: [], availability_ids: [], skill_ids: [], center_ids: [],
+    certification_ids: []
+    )
   end
 
 end

d711530ecc14393a76f39d251729241f5c3d184f
G115H <hillsgemma@gmail.com>
2017-02-18 20:57:42 +0800

    add email template for invited volunteers

--- app/mailers/new_event_mailer.rb
+++ app/mailers/new_event_mailer.rb
@@ -4,6 +4,13 @@ class NewEventMailer < ApplicationMailer
     @event = event
     @volunteer = volunteer
 
-    mail(to: @volunteer.email, subject: "New event by PPIS #{@event.event_name}")
+    mail(to: @volunteer.email, subject: "New event by PPIS: #{@event.event_name}")
+  end
+
+  def invite_volunteer(event, volunteer)
+    @event = event
+    @volunteer = volunteer
+
+    mail(to: @volunteer.email, subject: "Please join PPIS for #{@event.event_name}")
   end
 end

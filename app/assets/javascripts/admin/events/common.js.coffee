9521982acc75eebfd393f7abf1754f6a979b6fce
Sherwin <anthonysherwinchua@gmail.com>
2017-04-02 00:09:10 +0800

    convert event form to simple form

--- app/assets/javascripts/admin/events/common.js.coffee
+++ app/assets/javascripts/admin/events/common.js.coffee
@@ -1,6 +1,6 @@
 class PPIS.Views.Admin_Events.Common
   render: ->
-    $('.datetimepicker').datetimepicker
+    $('.datepicker').datetimepicker
       format: 'Do MMMM YYYY'
       extraFormats: [ 'DD.MM.YYYY', 'DD.MM.YY', 'YYYY-MM-DD' ]
       calendarWeeks: true
@@ -8,7 +8,7 @@ class PPIS.Views.Admin_Events.Common
       allowInputToggle: true
       useCurrent: true
 
-    $('.datetimepicker3').datetimepicker
+    $('.timepicker').datetimepicker
       format: 'HH:mm'
       extraFormats: [ 'YYYY-MM-DD HH:mm:ss' ]
       stepping: 15
--- app/models/event.rb
+++ app/models/event.rb
@@ -19,6 +19,8 @@ class Event < ApplicationRecord
   validates :number_of_volunteers, presence: true
   validates :duration, presence: true, numericality: { greater_than: 0 }
 
+  validate :start_time_must_be_before_end_time
+
   before_validation :calculate_duration
 
   aasm :column => 'state' do
@@ -39,6 +41,13 @@ class Event < ApplicationRecord
 
   def calculate_duration
     self.duration = end_time.to_i - start_time.to_i
+  end
+
+  def start_time_must_be_before_end_time
+    if start_time > end_time
+      errors.add(:start_time, 'must be before end time')
+      errors.add(:end_time, 'must be after start time')
+    end
   end
 
 end

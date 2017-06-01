6f1eddbf425a65dea777963bc4a078c10fb0127b
Sherwin <anthonysherwinchua@gmail.com>
2017-06-01 22:20:29 +0800

    add current_password to the permitted params

--- app/controllers/my/profiles_controller.rb
+++ app/controllers/my/profiles_controller.rb
@@ -20,7 +20,7 @@ class My::ProfilesController < My::BaseController
 
   def profile_params
     params.require(:volunteer).permit(
-      :name, :gender, :"date_of_birth(3i)", :"date_of_birth(2i)", :"date_of_birth(1i)",
+      :name, :gender, :"date_of_birth(3i)", :"date_of_birth(2i)", :"date_of_birth(1i)", :current_password,
       :mobile, :profession, :other_talents, :about_me, :email, :password, :password_confirmation, :formal_education_id,
       :marital_status, :address, :home_phone_number, :language_spoken, :past_experience, :nric_no, :adhoc, 
       :email_notification, target_group_ids: [], availability_ids: [], skill_ids: [], center_ids: [], 
